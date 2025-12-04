2df580d4af64690e6ec28cceb4097b92e787d751
Erubabu <erubabu081@gmail.com>
2025-12-04 12:49:59 -0500

    initial message commit

--- src/slices/chat3Slice.js
+++ src/slices/chat3Slice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    messages: [],
+    messages: [{sender: 'api', text: "Hello! How can i help you today!"}],
 }
 
 const chat3Slice = createSlice ({
